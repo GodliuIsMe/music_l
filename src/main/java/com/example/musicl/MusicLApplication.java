5e638ea4c6ac1ce0746046e73ee947719e6fcbf2
Liu <504588684@qq.com>
2022-03-15 08:44:49 +0800

    first commit

---
+++ src/main/java/com/example/musicl/MusicLApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicLApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicl/MusicLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
